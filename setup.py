d6a33479b5976c61fb84271a77e0f00d32398539
DuroCodes <davidwright13503@gmail.com>
2025-09-06 20:41:59 -0400

    chore: bump to v1.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="tablur",
-    version="1.2.0",
+    version="1.2.1",
     author="Duro",
     author_email="davidwright13503@gmail.com",
     description="a simple library for creating formatted tables with box-drawing characters",
